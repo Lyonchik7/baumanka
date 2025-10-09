8847a654d0882bc0928c934c5c81eae2475a9aa1
Lyonchik7 <154012140+Lyonchik7@users.noreply.github.com>
2025-10-10 00:00:32 +0300

    Add files via upload

---
+++ DzSeminar_5/Seminar_5(1,2).cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    // Задача 1
+    
+    const int n = 10;
+    vector<int> vec1(n);
+
+    int count = 1;
+    cout << "Вектор из 10 чисел от 1 до 10" << endl;
+
+    for_each(vec1.begin(), vec1.end(), [&count](int& element1) {
+        element1 = count++;
+        });
+
+    for_each(vec1.begin(), vec1.end(), [](int element1) {
+        cout << element1 << "  ";
+        });
+    cout << endl << endl;
+
+
+    // Задача 2
+
+    vector<int> vec2(n);
+    
+    cout << "Введите 10 элементов вектора (целые числа)" << endl;
+    for_each(vec2.begin(), vec2.end(), [](int& element2) {
+        cin >> element2;
+        });
+    
+    for_each(vec2.begin(), vec2.end(), [](int& element2) {
+        element2 = element2 * 2;
+        });
+    
+    cout << "Удвоенный вектор" << endl;
+    for_each(vec2.begin(), vec2.end(), [](int element2) {
+        cout << element2 << "  ";
+        });
+
+
+
+    return 0;
+}
+
---
+++ DzSeminar_5/Seminar_5(3).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    
+    const int n = 20;
+    vector<int> vec(n);
+    
+    cout << "Введите элементы вектора (20 целых чисел)" << endl;
+    for_each(vec.begin(), vec.end(), [](int& element) {
+        cin >> element;
+        });
+
+    cout << "Вектор:" << endl;
+    for_each(vec.begin(), vec.end(), [](int element) {
+        cout << element << "  ";
+        });
+
+    auto max1 = max_element(vec.begin(), vec.end());  // Первая более оптимизированная запись
+
+    auto max2 = max_element(vec.begin(), vec.end(),  // Вторая запись
+        [](int a, int b) {
+            return a < b;  
+        }
+    );
+
+    cout << "\nМаксимум: " << *max1 << endl;
+    cout << "\nМаксимум: " << *max2;
+
+    return 0;
+}
+
