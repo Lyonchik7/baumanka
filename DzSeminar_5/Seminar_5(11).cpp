cf4e2cd165357ffd5b59c65ce9cd00f84a2930d3
Lyonchik7 <154012140+Lyonchik7@users.noreply.github.com>
2025-10-17 00:08:21 +0300

    Add files via upload

---
+++ DzSeminar_5/Seminar_5(11).cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <numeric>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    const int n = 50;
+    vector<double> amplitude(n);
+
+    // Инициализация генератора случайных чисел
+    srand(time(0));
+
+    cout << "Амплитуды вибрации:" << endl;
+    for (int i = 0; i < n; i++) {
+        amplitude[i] = (rand() % 1000) / 1000.0; // значения от 0 до 1
+    }
+
+    for_each(amplitude.begin(), amplitude.end(), [](double elements) {
+        cout << elements << ", ";
+        });
+    cout << endl;
+
+    // 1) Удаление шумов (значения < 0.1)
+    amplitude.erase(remove_if(amplitude.begin(), amplitude.end(),
+        [](double value) { return value < 0.1; }),
+        amplitude.end());
+
+    cout << "\nАмплитуды после удаления шумов:" << endl;
+    for_each(amplitude.begin(), amplitude.end(), [](double elements) {
+        cout << elements << ", ";
+        });
+    cout << endl;
+
+    // 2) Нахождение среднего b максимального значениq амплитуды
+    double sum = accumulate(amplitude.begin(), amplitude.end(), 0.0);
+    double sredn = sum / amplitude.size();
+    cout << "\nСреднее значение амплитуды: " << sredn << endl;
+        
+    auto max = max_element(amplitude.begin(), amplitude.end());
+    cout << "\nМаксимальное значение амплитуды: " << *max << endl;
+
+    // 3) Проверка, превышают ли все значения порог 0.5
+    bool all_more = all_of(amplitude.begin(), amplitude.end(),
+        [](double value) { return value > 0.5; });
+    if (all_more) {
+        cout << "\nВсе значения превышают порог 0.5: ДА";
+    }
+    else {
+        cout << "\nВсе значения превышают порог 0.5: НЕТ";
+    }
+
+    // 4) Вывод 10 наибольших амплитуд для анализа
+    vector<double> sorted_amplitude = amplitude;
+    sort(sorted_amplitude.begin(), sorted_amplitude.end(), greater<int>());   // greater<int>() сортирует по убыванию
+
+    cout << "\n10 максимальных скоростей:" << endl;
+    for (int i = 0; i < 10 && i < sorted_amplitude.size(); i++) {
+        cout << sorted_amplitude[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ DzSeminar_5/Seminar_5(13).cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    const int n = 50;
+    vector<double> pressure(n);
+
+    // Инициализация генератора случайных чисел
+    srand(time(0));
+
+    cout << "Давление в трубопроводе:" << endl;
+    for (int i = 0; i < n; i++) {
+        pressure[i] = 1 + (rand() % 1000) / 100.0; // значения от 1 до 10 бар
+    }
+
+    for_each(pressure.begin(), pressure.end(), [](double elements) {
+        cout << elements << ", ";
+        });
+    cout << endl;
+
+    // 1) Удаление ошибочных значений (< 0 или > 12)
+    pressure.erase(remove_if(pressure.begin(), pressure.end(),
+        [](double value) { return value < 0 || value > 12; }),
+        pressure.end());
+
+    cout << "\nДавление после удаления ошибочных значений:" << endl;
+    for_each(pressure.begin(), pressure.end(), [](double elements) {
+        cout << elements << ", ";
+        });
+    cout << endl;
+
+    // 2) Вычисление среднего давления
+    double average = accumulate(pressure.begin(), pressure.end(), 0.0) / pressure.size();
+    cout << "Среднее давление: " << average << " бар" << endl;
+
+    // 3)) Нахождение минимального и максимального давления
+    auto minmax = minmax_element(pressure.begin(), pressure.end());
+    cout << "Минимальное давление: " << *minmax.first << " бар" << endl;
+    cout << "Максимальное давление: " << *minmax.second << " бар" << endl;
+
+    // 4) Вывод значений, превышающих 8 бар
+    cout << "\nЗначения, превышающие 8 бар:" << endl;
+    for_each(pressure.begin(), pressure.end(), [](double value) {
+        if (value > 8) {
+            cout << value << ", ";
+        }
+        });
+
+    return 0;
+}
