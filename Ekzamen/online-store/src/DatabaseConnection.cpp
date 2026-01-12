787f5bce088a9136d32c5c0b946bf83e00457e96
Lyonchik7 <154012140+Lyonchik7@users.noreply.github.com>
2026-01-12 18:26:43 +0300

    Update DatabaseConnection.cpp

--- Ekzamen/online-store/src/DatabaseConnection.cpp
+++ Ekzamen/online-store/src/DatabaseConnection.cpp
@@ -1,11 +1,10 @@
-#include "DatabaseConnection.hpp"
+#include "DatabaseConnection.h"
 #include <iostream>
 
 using namespace std;
 
-// Конструктор
-template<typename T>
-DatabaseConnection<T>::DatabaseConnection(const string& connStr) {
+// Конструктор - подключение к БД
+DatabaseConnection<string>::DatabaseConnection(const string& connStr) {
     try {
         conn = make_unique<pqxx::connection>(connStr);
         if (!conn->is_open()) {
@@ -18,24 +17,22 @@ DatabaseConnection<T>::DatabaseConnection(const string& connStr) {
 }
 
 // Деструктор
-template<typename T>
-DatabaseConnection<T>::~DatabaseConnection() {
+DatabaseConnection<string>::~DatabaseConnection() {
     if (conn && conn->is_open()) {
         conn->close();
     }
 }
 
-// Выполнение запроса
-template<typename T>
-vector<vector<T>> DatabaseConnection<T>::executeQuery(const string& query) {
-    vector<vector<T>> result;
+// Выполнить запрос с результатом
+vector<vector<string>> DatabaseConnection<string>::executeQuery(const string& sql) {
+    vector<vector<string>> result;
     
     try {
         pqxx::nontransaction ntx(*conn);
-        pqxx::result res = ntx.exec(query);
+        pqxx::result res = ntx.exec(sql);
         
         for (const auto& row : res) {
-            vector<T> rowData;
+            vector<string> rowData;
             for (const auto& field : row) {
                 rowData.push_back(field.c_str());
             }
@@ -48,64 +45,54 @@ vector<vector<T>> DatabaseConnection<T>::executeQuery(const string& query) {
     return result;
 }
 
-// Выполнение команды без результата
-template<typename T>
-void DatabaseConnection<T>::executeNonQuery(const string& query) {
+// Выполнить команду без результата
+void DatabaseConnection<string>::executeNonQuery(const string& sql) {
     try {
         pqxx::work w(*conn);
-        w.exec(query);
+        w.exec(sql);
         w.commit();
     } catch (const exception& e) {
         throw runtime_error(string("Command error: ") + e.what());
     }
 }
 
-// Начало транзакции
-template<typename T>
-void DatabaseConnection<T>::beginTransaction() {
+// Начать транзакцию
+void DatabaseConnection<string>::beginTransaction() {
     try {
         if (transaction) {
             transaction->abort();
         }
         transaction = make_unique<pqxx::work>(*conn);
     } catch (...) {
-        // Игнорируем ошибку
+        cerr << "Transaction begin error\n";
     }
 }
 
-// Завершение транзакции
-template<typename T>
-void DatabaseConnection<T>::commitTransaction() {
+// Завершить транзакцию
+void DatabaseConnection<string>::commitTransaction() {
     try {
         if (transaction) {
             transaction->commit();
             transaction.reset();
         }
     } catch (...) {
-        // Игнорируем ошибку
+        cerr << "Transaction commit error\n";
     }
 }
 
-// Откат транзакции
-template<typename T>
-void DatabaseConnection<T>::rollbackTransaction() {
+// Откатить транзакцию
+void DatabaseConnection<string>::rollbackTransaction() {
     try {
         if (transaction) {
             transaction->abort();
             transaction.reset();
         }
     } catch (...) {
-        // Игнорируем ошибку
+        cerr << "Transaction rollback error\n";
     }
 }
 
-// Проверка подключения
-template<typename T>
-bool DatabaseConnection<T>::isConnected() const {
+// Проверить подключение
+bool DatabaseConnection<string>::isConnected() const {
     return conn && conn->is_open();
 }
-
-// Явная инстанциация шаблона
-template class DatabaseConnection<string>;
-template class DatabaseConnection<int>;
-template class DatabaseConnection<double>;
